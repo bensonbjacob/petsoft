d74a0674f955769691fc676c91e3ddfc7ae7c043
Jacob <jacob.benson.dev@gmail.com>
2024-02-29 20:02:16 -0600

    Enable custom image domains and update homepage with new logo
    Expanded the Next.js configuration to support images from specific remote hosts, enhancing our ability to inline images from trusted domains by defining 'remotePatterns'. Additionally, introduced a freshly designed SVG logo along with modifications to the marketing homepage to include the new branding. This also entailed implementing a Logo component for consistent header visualization across the platform.
    
    A layout refresh on the homepage now showcases the logo and provides a better visual context for PetSoft's services, with emphasis on the new branding.

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,29 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of PetSoft"
+        width={519}
+        height={472}
+      />
+
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage with your{" "}
+          <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Dolores debitis enim ea ullam architecto unde quo.
+        </p>
+        <div className="mt-10"></div>
+      </div>
+    </main>
+  );
 }
