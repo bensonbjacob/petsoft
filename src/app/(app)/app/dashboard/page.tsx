ec00ce40239e8c26abc425b3e61cae5d4824c56c
Jacob <jacob.benson.dev@gmail.com>
2024-03-03 14:26:23 -0600

    Enhance dashboard with branding and stats
    Introduced two new components, 'Branding' and 'Stats', to the dashboard page, enriching the UI with company branding and current guest statistics. Created a 'H1' component for typographic consistency across the branding elements. This update aims to improve user experience by providing immediate, valuable information at a glance.

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <div className="flex justify-between items-center text-white py-8">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
