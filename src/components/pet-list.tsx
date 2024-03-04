84761b9c9a29a8438fa5c87b6473368b8ff8af11
Jacob <jacob.benson.dev@gmail.com>
2024-03-04 12:41:12 -0600

    Highlight selected pet in PetList component
    Enhanced visual feedback in the PetList by dynamically assigning a background color to the currently selected pet item. Leveraging context, the comparison between each pet id and the selectedPetId dictates the application of the highlight style, improving user experience by visually confirming their selection. Utilized `cn` utility function for conditional className application without inline style complexity.

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function PetList() {
-  const { pets, handleChangeSelectedPetId } = usePetContext();
+  const { pets, selectedPetId, handleChangeSelectedPetId } =
+    usePetContext();
 
   return (
     <ul className="bg-white border-b border-black/[0.08]">
@@ -12,7 +14,12 @@ export default function PetList() {
         <li key={pet.id}>
           <button
             onClick={() => handleChangeSelectedPetId(pet.id)}
-            className="flex h-[70px] cursor-pointer w-full items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition">
+            className={cn(
+              "flex h-[70px] cursor-pointer w-full items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition",
+              {
+                "bg-[#eff1f2]": selectedPetId === pet.id,
+              }
+            )}>
             <Image
               src={pet.imageUrl}
               alt="Pet picture"
