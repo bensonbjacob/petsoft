8060a100039f802be2a90416649ebd0924fdd2f4
Jacob <jacob.benson.dev@gmail.com>
2024-03-03 21:04:07 -0600

    Implement global pet state using context
    Refactor pet data fetching to use a global context, optimizing component reusability across the application. Removed direct API calls from the Dashboard component, introducing a PetContextProvider to handle data fetching and state management. This allows for pet data to be accessed via a custom hook, standardizing pet data access patterns and reducing the need for prop drilling. A new context and hook (`PetContext` and `usePetContext`) have been created to facilitate this pattern. The change ensures better separation of concerns and potentially improves performance by centralizing pet state management.

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,18 +1,31 @@
 import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
+import PetContextProvider from "@/contexts/pet-context-provider";
+import { Pet } from "@/lib/types";
 
-export default function Layout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch pets");
+  }
+
+  const data: Pet[] = await response.json();
   return (
     <>
       <BackgroundPattern />
       <div className="max-w-[1050px] mx-auto px-4 flex flex-col min-h-screen">
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>
+          {children}
+        </PetContextProvider>
         <AppFooter />
       </div>
     </>
