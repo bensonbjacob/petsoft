d5c5b041614b711bedae500e2e9382acf51914de
Jacob <jacob.benson.dev@gmail.com>
2024-03-06 16:49:43 -0600

    Enhanced pet form dialog interaction
    Introduced local state management to control the pet form dialog visibility in the PetButton component, improving the user's interaction flow. Stateful handling enables the dialog to respond appropriately to user actions. Now, the dialog closes upon successful pet form submission, creating a seamless experience. Additionally, adhered to strict mode compatibility by utilizing the 'use strict' directive.

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import {
@@ -8,6 +10,7 @@ import {
   DialogTrigger,
 } from "./ui/dialog";
 import PetForm from "./pet-form";
+import { useState } from "react";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
@@ -20,6 +23,8 @@ export default function PetButton({
   children,
   onClick,
 }: PetButtonProps) {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   if (actionType === "checkout") {
     return (
       <Button variant="secondary" onClick={onClick}>
@@ -29,7 +34,7 @@ export default function PetButton({
   }
 
   return (
-    <Dialog>
+    <Dialog open={isFormOpen} onOpenChange={setIsFormOpen}>
       <DialogTrigger asChild>
         {actionType === "add" ? (
           <Button size="icon">
@@ -46,7 +51,10 @@ export default function PetButton({
             {actionType === "add" ? "Add a new pet" : "Edit Pet"}
           </DialogTitle>
         </DialogHeader>
-        <PetForm actionType={actionType} />
+        <PetForm
+          actionType={actionType}
+          onFormSubmission={() => setIsFormOpen(false)}
+        />
       </DialogContent>
     </Dialog>
   );
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -8,9 +8,13 @@ import { usePetContext } from "@/lib/hooks";
 
 type PetFormProps = {
   actionType: "add" | "edit";
+  onFormSubmission: () => void;
 };
 
-export default function PetForm({ actionType }: PetFormProps) {
+export default function PetForm({
+  actionType,
+  onFormSubmission,
+}: PetFormProps) {
   const { handleAddPet } = usePetContext();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -28,6 +32,8 @@ export default function PetForm({ actionType }: PetFormProps) {
     };
 
     handleAddPet(newPet);
+
+    onFormSubmission();
   };
 
   return (
