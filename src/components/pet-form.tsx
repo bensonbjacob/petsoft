4100a540cb952e80cce3c4153363dafeffca33e4
Jacob <jacob.benson.dev@gmail.com>
2024-03-06 16:02:05 -0600

    Add pet form UI components and update button logic
    Implemented a detailed pet form with new UI components like labels, inputs, and text areas, allowing for user interaction in creating or updating pet information. Updated the PetButton component to pass the action type to the PetForm, enabling dynamic button labeling for 'Add' or 'Update' actions. This change also included adding the @radix-ui/react-label package for improved accessibility and consistent styling across form elements. The addition complements the existing UI library and is expected to enhance user experience on the pet management interface.

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,5 +1,54 @@
 import React from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
-export default function PetForm() {
-  return <form></form>;
+type PetFormProps = {
+  actionType: "add" | "edit";
+};
+
+export default function PetForm({ actionType }: PetFormProps) {
+  return (
+    <form className="flex flex-col">
+      <div className="space-y-3">
+        <div className="space-y-1">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" placeholder="Enter pet name" type="text" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="ownerName">Owner name</Label>
+          <Input
+            id="ownerName"
+            placeholder="Enter owner name"
+            type="text"
+          />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="imageUrl">Image Url</Label>
+          <Input
+            id="imageUrl"
+            placeholder="Enter image url"
+            type="text"
+          />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="age">Age</Label>
+          <Input id="age" placeholder="Enter pet age" type="number" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="notes">Notes</Label>
+          <Textarea id="notes" rows={4} placeholder="Enter notes" />
+        </div>
+      </div>
+
+      <Button className="mt-5 self-end" type="submit">
+        {actionType === "add" ? "Add" : "Update"}
+      </Button>
+    </form>
+  );
 }
