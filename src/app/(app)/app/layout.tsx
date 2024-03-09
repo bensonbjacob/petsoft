e8899ec10634a0bff76d47af5f355c15db4c3042
Jacob <jacob.benson.dev@gmail.com>
2024-03-08 19:10:31 -0600

    Refactor pet data fetching to use Prisma
    Replaced the external API call for fetching pet information with direct queries to the database using Prisma. This enhances data retrieval performance and reliability by leveraging our internal database instead of depending on a third-party service. The change simplifies the codebase, reducing network overhead, and potential points of failure.

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,29 +3,22 @@ import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PetContextProvider from "@/contexts/pet-context-provider";
 import SearchContextProvider from "@/contexts/search-context-provider";
-import { Pet } from "@/lib/types";
+import prisma from "@/lib/db";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
+  const pets = await prisma.pet.findMany();
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch pets");
-  }
-
-  const data: Pet[] = await response.json();
   return (
     <>
       <BackgroundPattern />
       <div className="max-w-[1050px] mx-auto px-4 flex flex-col min-h-screen">
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider data={data}>
+          <PetContextProvider data={pets}>
             {children}
           </PetContextProvider>
         </SearchContextProvider>
