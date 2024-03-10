39e6dec48bd097812094e19569a3c65d0de332f6
Jacob <jacob.benson.dev@gmail.com>
2024-03-09 20:52:14 -0600

    Enhance button disable logic and streamline types
    The PetButton component now supports a 'disabled' prop to allow for stateful disabling, improving UX during longer operations like pet checkout. In the PetDetails component, the pet checkout process now employs React's useTransition for better state handling, allowing the UI to reflect pending states. The Pet type has been centralized to use the Prisma client definition, removing redundancy and ensuring consistency across the codebase.

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -4,6 +4,8 @@ import { usePetContext } from "@/lib/hooks";
 import Image from "next/image";
 import PetButton from "./pet-button";
 import { deletePet } from "@/actions/actions";
+import { Pet } from "@prisma/client";
+import { useTransition } from "react";
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
@@ -39,6 +41,7 @@ type Props = {
 
 function TopBar({ pet }: Props) {
   const { handleCheckoutPet } = usePetContext();
+  const [isPending, startTransition] = useTransition();
 
   return (
     <div className="flex items-center bg-white px-8 py-5 border-b border-light">
@@ -58,7 +61,12 @@ function TopBar({ pet }: Props) {
         <PetButton actionType="edit">Edit</PetButton>
         <PetButton
           actionType="checkout"
-          onClick={async () => await deletePet(pet.id)}>
+          disabled={isPending}
+          onClick={async () => {
+            startTransition(async () => {
+              await deletePet(pet.id);
+            });
+          }}>
           Checkout
         </PetButton>
       </div>
