9b72c3d5881bcaa8041cf48a9b6e38bb9ee4ce95
Jacob <jacob.benson.dev@gmail.com>
2024-03-17 13:59:45 -0500

    Ensure string typing for user IDs in auth tokens
    Introduced explicit casting of user IDs to strings in the JWT creation process to align with type expectations. Updated the session handling to safely assign user IDs only when the session object's user property is present. Added TypeScript declaration file for 'next-auth' to enhance type safety and ensure the JWT and Session interfaces reflect our custom properties, which will improve developer experience and reduce runtime errors.

---
+++ src/lib/next-auth.d.ts
@@ -0,0 +1,20 @@
+import {} from "next-auth";
+
+// declare module "next-auth" {
+//   interface User {
+//     hasAccess: boolean;
+//     email: string;
+//   }
+
+//   interface Session {
+//     user: User & {
+//       id: string;
+//     };
+//   }
+// }
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    userId: string;
+  }
+}
