4db0b19726839a128b073f28b63c8fb5f4d66bee
Jacob <jacob.benson.dev@gmail.com>
2024-03-24 11:50:22 -0500

    Refactor payment page to conditionally render success message

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -4,18 +4,24 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default function Page({ searchParams }) {
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1 className="mt-2">PetSoft access requires payment.</H1>
 
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for $299.
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession();
+          }}
+        >
+          Buy lifetime access for $299.
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <p className="text-sm text-green-700">Payment successful!</p>
+      )}
     </main>
   );
 }
