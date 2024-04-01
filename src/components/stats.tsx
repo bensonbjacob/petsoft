f8bc94a0090b2d1501764fc4a3cf84f2515a82b4
Jacob <jacob.benson.dev@gmail.com>
2024-03-31 21:11:31 -0500

    Fix variable name in Stats component

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -3,10 +3,10 @@
 import { usePetContext } from "@/lib/hooks";
 
 export default function Stats() {
-  const { numberofPets } = usePetContext();
+  const { numberOfPets } = usePetContext();
   return (
     <section className="text-center">
-      <p className="text-2xl font-bold leading-6">{numberofPets}</p>
+      <p className="text-2xl font-bold leading-6">{numberOfPets}</p>
       <p className="opacity-80">current guests</p>
     </section>
   );
